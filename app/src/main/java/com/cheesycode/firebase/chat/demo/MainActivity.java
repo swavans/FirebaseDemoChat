41477777c62cf4f98a1aba7891bcac0b9d97819f
Swavans <sworms@avans.nl>
2018-12-11 18:08:22 +0100

    Demo empty

--- app/src/main/java/com/cheesycode/firebase/chat/demo/MainActivity.java
+++ app/src/main/java/com/cheesycode/firebase/chat/demo/MainActivity.java
@@ -1,42 +1,18 @@
 package com.cheesycode.firebase.chat.demo;
 
-import android.content.Context;
 import android.content.Intent;
-import android.support.annotation.NonNull;
-import android.support.design.widget.TextInputEditText;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.EditText;
-import android.widget.Toast;
-
-import com.firebase.ui.auth.AuthUI;
-import com.firebase.ui.auth.IdpResponse;
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.OnFailureListener;
-import com.google.android.gms.tasks.OnSuccessListener;
-import com.google.android.gms.tasks.Task;
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.firestore.FirebaseFirestore;
-import com.google.firebase.firestore.QueryDocumentSnapshot;
-import com.google.firebase.firestore.QuerySnapshot;
-import com.google.firebase.messaging.FirebaseMessaging;
-import com.google.firebase.messaging.RemoteMessage;
-
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     private static final int RC_SIGN_IN = 123;
-    private TextInputEditText message;
+    private EditText message;
     private EditText messageHistory;
-    private EditText other_user;
+    private TextView other_user;
     private Conversation lastConversation;
     private String documentId = "";
 
@@ -78,23 +54,23 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void SignOut() {
-        AuthUI.getInstance()
-                .signOut(this)
-                .addOnCompleteListener(new OnCompleteListener<Void>() {
-                    public void onComplete(@NonNull Task<Void> task) {
-                        // ...
-                    }
-                });
+//        AuthUI.getInstance()
+//                .signOut(this)
+//                .addOnCompleteListener(new OnCompleteListener<Void>() {
+//                    public void onComplete(@NonNull Task<Void> task) {
+//                        // ...
+//                    }
+//                });
     }
 
     public void DeleteUser() {
-        AuthUI.getInstance()
-                .delete(this)
-                .addOnCompleteListener(new OnCompleteListener<Void>() {
-                    @Override
-                    public void onComplete(@NonNull Task<Void> task) {
-                        // ...
-                    }
-                });
+//        AuthUI.getInstance()
+//                .delete(this)
+//                .addOnCompleteListener(new OnCompleteListener<Void>() {
+//                    @Override
+//                    public void onComplete(@NonNull Task<Void> task) {
+//                        // ...
+//                    }
+//                });
     }
 }
--- app/src/main/java/com/cheesycode/firebase/chat/demo/MessageService.java
+++ app/src/main/java/com/cheesycode/firebase/chat/demo/MessageService.java
@@ -10,46 +10,18 @@ import android.support.v4.app.NotificationCompat;
 import android.support.v4.app.NotificationManagerCompat;
 import android.util.Log;
 
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
-import com.google.firebase.messaging.FirebaseMessagingService;
-import com.google.firebase.messaging.RemoteMessage;
 
-public class MessageService extends FirebaseMessagingService {
 
-    @Override
+public class MessageService  {
+
+
     public void onNewToken(String token) {
         Log.d("MESSAGING_TOKEN", "Refreshed token: " + token);
         createNotificationChannel();
     }
 
-    @Override
-    public void onMessageReceived(RemoteMessage remoteMessage) {
-        Log.d("NEW_MESSASGE", "From: " + remoteMessage.getFrom());
-        NotificationCompat.Builder mBuilder = new NotificationCompat.Builder(this, getResources().getString(R.string.channel_id))
-                .setSmallIcon(R.mipmap.ic_launcher)
-                .setContentTitle("Test")
-                .setLargeIcon(BitmapFactory.decodeResource(getResources(), R.mipmap.ic_launcher))
-                .setContentText(remoteMessage.getData().toString())
-                .setPriority(NotificationCompat.PRIORITY_DEFAULT)
-                .setAutoCancel(true);
-        String message = remoteMessage.getData().get("test").toString();
 
-        Intent pushReceivedIntent = new Intent(getApplicationContext(), PushNotificationReceived.class);
-
-        pushReceivedIntent.putExtra("message", message);
-
-        PendingIntent pendingIntent = PendingIntent.getActivity(getApplicationContext(), 0, pushReceivedIntent, PendingIntent.FLAG_UPDATE_CURRENT);
-
-        mBuilder.setContentIntent(pendingIntent);
-
-
-
-        NotificationManagerCompat notificationManager = NotificationManagerCompat.from(this);
-
-        notificationManager.notify(11, mBuilder.build());
-
-
+    public void onMessageReceived() {
     }
 
     private void createNotificationChannel() {
